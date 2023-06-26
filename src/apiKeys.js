408ee80897ce437fe46c802968c3311cc26c4d2a
Anushka Singh <101061044+anushkaSingh30xxx@users.noreply.github.com>
2023-06-26 08:41:14 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "7927fad48d40434002376d59e5c31b1a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
